fe9f9604becc2cddc6198fb56ae1b6618cb25172
Xavi002 <diogomartins200214@gmail.com>
2020-10-14 14:45:27 +0100

    BACK-END: creating orphanage on database

--- back-end/src/database/migrations/1602680934148-create_orphanages.ts
+++ back-end/src/database/migrations/1602680934148-create_orphanages.ts
@@ -38,6 +38,10 @@ export class createOrphanages1602680934148 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,11 +1,37 @@
 import express from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from './models/Orphanage'
 
 import './database/connection'
 
 const app = express()
 app.use(express.json())
 
-app.get('/', (request, response) => {
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+   } = request.body
+
+   const orphanagesRepository = getRepository(Orphanage)
+
+   const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+   })
+
+   await orphanagesRepository.save(orphanage)
+
   return response.json({ message: 'Hello World' })
 })
 
